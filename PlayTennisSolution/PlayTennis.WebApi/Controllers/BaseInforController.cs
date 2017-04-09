fa41ecec42675f77cef915d316eb7924c30e86f2
sangeren <ch199108@gmail.com>
2017-04-10 00:10:28 +0800

    基本信息

---
+++ PlayTennisSolution/PlayTennis.WebApi/Controllers/BaseInforController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PlayTennis.Model;
+
+namespace PlayTennis.WebApi.Controllers
+{
+    public class BaseInforController : ApiController
+    {
+        // GET: api/BaseInfor
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/BaseInfor/5
+        public UserBaseInfo Get(int id)
+        {
+            return new UserBaseInfo();
+        }
+
+        // POST: api/BaseInfor
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/BaseInfor/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/BaseInfor/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
