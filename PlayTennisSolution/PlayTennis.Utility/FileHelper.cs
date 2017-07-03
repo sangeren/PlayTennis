9e4a8e86453259e9c184310ac8fd29e891987ed4
sangeren <ch199108@gmail.com>
2017-07-04 00:17:21 +0800

    添加上传图片

---
+++ PlayTennisSolution/PlayTennis.Utility/FileHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace PlayTennis.Utility
+{
+    public class FileHelper
+    {
+        private static log4net.ILog _log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public static Tuple<bool, string> SaveSingleFile(HttpPostedFile file)
+        {
+            try
+            {
+                var path = ConfigHelper.GetConfigValueOrDefault("imageFilePath", @"C:\webLogs\image\");
+                if (Directory.Exists(path) == false)//如果不存在就创建file文件夹
+                {
+                    Directory.CreateDirectory(path);
+                }
+                var fileSaveName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+
+                file.SaveAs(path + fileSaveName);
+
+                return new Tuple<bool, string>(true, fileSaveName);
+            }
+            catch (Exception ex)
+            {
+                _log.Error(JsonConvert.SerializeObject(ex));
+                return new Tuple<bool, string>(false, ex.Message);
+            }
+        }
+    }
+}
---
+++ PlayTennisSolution/PlayTennis.WebApi/Controllers/UploadFileController.cs
@@ -0,0 +1,45 @@
+﻿using PlayTennis.Utility;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace PlayTennis.WebApi.Controllers
+{
+    public class UploadFileController : ApiController
+    {
+        // GET: api/UploadFile
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/UploadFile/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/UploadFile
+        public string Post()
+        {
+            var imageFile = HttpContext.Current.Request.Files["image1"];
+            var result = FileHelper.SaveSingleFile(imageFile);
+            return result.Item2;
+        }
+
+        // PUT: api/UploadFile/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/UploadFile/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
